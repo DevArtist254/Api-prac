556736a06670da68a55625db192d55861a978daa
devartist <kevinwasonga415@outlook.com>
2023-02-08 04:18:51 +0300

    env

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const tourRouter = require("./routes/tourRouters")
+const morgan = require("morgan")
 
 const app = express()
 
@@ -7,6 +8,10 @@ const app = express()
 //Gobal Middlewares
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
+
+if (process.env.NODE_ENV === "development") {
+ app.use(morgan("dev"))
+}
 
 //Gobal middle to be used down the pipeline
 app.use((req, res, next) => {
