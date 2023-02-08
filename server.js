b988a5049b0738d1de33879f11bff9df77c51e2b
devartist <kevinwasonga415@outlook.com>
2023-02-08 03:26:15 +0300

    DB setup

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+require("dotenv").config({path: "./config.env"})
+
 ////////////////////////////////////////
 //Server
+const mongoose = require("mongoose")
 const app = require("./app")
+
+const DB = process.env.DB_CONNECT.replace("<PASSWORD>", process.env.DB_PASSWORD)
+
+mongoose
+ .connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+ })
+ .then(() => {
+  console.log(`DB Connection was success`)
+ })
 
 const port = 3000 || process.env.port
 
