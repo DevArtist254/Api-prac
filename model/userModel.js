df31431ac870dd3fc8d95a7fad79fe717c98f0cc
devartist <kevinwasonga415@outlook.com>
2023-03-31 04:16:03 +0300

    ces

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,7 +27,27 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //Checker - true or false
+      validator: function (el) {
+        //if false it's going to return our validation error message
+        return el === this.password;
+      },
+      //message
+      message: 'Please confirm your password',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //space between getting the data and saving the data
+  if (!this.isModified('password')) return next();
+
+  //Hash with cost of 12
+  this.password = await bcryptjs.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
